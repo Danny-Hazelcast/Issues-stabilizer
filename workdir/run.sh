0b54d41e11028a0277c7738af914658e7a8c78b9
Peter Veentjer <alarmnummer@gmail.com>
2014-05-16 20:49:47 +0300

    fixed the jvm parameters

--- workdir/run.sh
+++ workdir/run.sh
@@ -1,18 +1,16 @@
-
 #!/bin/bash
 
 provisioner --scale 3
 
-coordinator     --workerVmOptions "-server -Xms1800m -Xmx3584m -Djava.awt.headless=true -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps -Xloggc:$
+coordinator     --workerVmOptions "-server -Xms1800m -Xmx3584m -Djava.awt.headless=true -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps -Xloggc:gc.log -verboseg -XX:+PrintGCDetails -XX:+PrintTenuringDistribution -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:MaxPermSize=300m"
                 --clientHzFile      ../conf/client-hazelcast.xml \
                 --hzFile            ../conf/hazelcast.xml \
                 --clientWorkerCount 0 \
                 --memberWorkerCount 3 \
                 --workerClassPath   '../target/*.jar' \
-                --duration          2h \
+                --duration          5m \
                 ../conf/test.properties
 
 provisioner --download
 
 provisioner --terminate
-
