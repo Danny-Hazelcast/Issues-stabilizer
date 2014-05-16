10682c83e18a112972400215e1c203a460f4faba
Peter Veentjer <alarmnummer@gmail.com>
2014-05-16 18:53:49 +0300

    Update run.sh

--- workingdir/run.sh
+++ workingdir/run.sh
@@ -1,14 +1,15 @@
+
 #!/bin/bash
 
-provisioner --scale 4
+provisioner --scale 3
 
-coordinator     --workerVmOptions "-ea -server -Xms2G -Xmx2G -XX:+PrintGC -XX:+HeapDumpOnOutOfMemoryError" \
+coordinator     --workerVmOptions "-server -Xms1800m -Xmx3584m -Djava.awt.headless=true -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps -Xloggc:$
                 --clientHzFile      ../conf/client-hazelcast.xml \
                 --hzFile            ../conf/hazelcast.xml \
                 --clientWorkerCount 0 \
                 --memberWorkerCount 3 \
                 --workerClassPath   '../target/*.jar' \
-                --duration          5m \
+                --duration          2h \
                 ../conf/test.properties
 
 provisioner --download
