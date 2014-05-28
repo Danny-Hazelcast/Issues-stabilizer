04ff02ccb1a868ee5ddc2b9a72c538a429ccd017
danny <danny@hazelcast.com>
2014-05-28 17:11:49 +0300

    run.sh

--- workdir/run.sh
+++ workdir/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-provisioner --scale 3
+provisioner --scale 4
 
 coordinator     --workerVmOptions "-server -Xms1800m -Xmx3584m -Djava.awt.headless=true -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps -Xloggc:gc.log -verboseg -XX:+PrintGCDetails -XX:+PrintTenuringDistribution -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:MaxPermSize=300m" \
                 --clientHzFile      ../conf/client-hazelcast.xml \
                 --hzFile            ../conf/hazelcast.xml \
-                --clientWorkerCount 0 \
-                --memberWorkerCount 3 \
+                --clientWorkerCount 2 \
+                --memberWorkerCount 2 \
                 --workerClassPath   '../target/*.jar' \
-                --duration          5m \
+                --duration          30m \
                 ../conf/test.properties
 
 provisioner --download
